5242f5a9659ef44baac39326d75e67d2536aa44e
maddy-1 <maddy.flynn@icloud.com>
2021-12-29 20:39:57 +0000

    phpless goings on

--- postserver.php
+++ postserver.php
@@ -1,11 +1,44 @@
-</php*<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Void and Null</title>
-</head>
-<body>
-  Is there anyone there?
-</body>
-</html>
+<?php
+
+function clean($data){
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+function IsChecked($chkname,$value){
+  if(!empty($_POST[$chkname]))
+    {
+      foreach($_POST[$chkname] as $chkval)
+        {
+          if($chkval == $value)
+            {
+              return true;
+            }
+        }
+    }
+  return false;
+};
+    
+if (isset($_POST['submit'])) {
+  $start_point = clean($_POST['start_point']);
+  $date = clean($_POST['date']);
+	$lat = clean($_POST['latitude']);
+	$long = clean($_POST['longitude']);
+	$main_text = clean($_POST['main_text']);
+  $image = clean($_POST['image']);
+	$sun = IsChecked('weather','sun') ? 1 : 0;
+	$rain = IsChecked('weather','rain') ? 1 : 0;
+	$wind = IsChecked('weather','wind') ? 1 : 0;
+	$cloud = IsChecked('weather','cloud') ? 1 : 0;
+	$snow = IsChecked('weather','snow') ? 1 : 0;
+  $lightning = IsChecked('weather','lightning') ? 1 : 0;
+  $hail = IsChecked('weather','hail') ? 1 : 0;
+
+
+};
+
+
+
+?>
